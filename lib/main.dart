6e5581b063882f6b6102458f8cdd6a02bde10ccf
joaovelozo <joaovelozo@gmail.com>
2023-01-28 23:17:13 -0300

    update_main

--- lib/main.dart
+++ lib/main.dart
@@ -4,8 +4,8 @@ import 'package:sqflite_common_ffi/sqflite_ffi.dart';
 import 'package:sqflite/sqflite.dart';
 
 void main() {
-  //sqfliteFfiInit();
-  //databaseFactory = databaseFactoryFfi;
+  sqfliteFfiInit();
+  databaseFactory = databaseFactoryFfi;
   runApp(const MyApp());
 }
 
